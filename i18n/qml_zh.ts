741584275b597291e2ab0e978c154fd0a150dbb2
eton <bbaodi@ace-cardio.com>
2025-03-27 14:33:00 +0800

    add ts files

---
+++ i18n/qml_zh.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Qmli18n</name>
+    <message>
+        <location filename="Qmli18n.qml" line="24"/>
+        <source>Hello</source>
+        <translatorcomment>你好</translatorcomment>
+        <translation>你好</translation>
+    </message>
+    <message>
+        <location filename="Qmli18n.qml" line="30"/>
+        <source>English</source>
+        <translatorcomment>英语</translatorcomment>
+        <translation>英语</translation>
+    </message>
+    <message>
+        <location filename="Qmli18n.qml" line="38"/>
+        <source>id-French</source>
+        <translatorcomment>法语</translatorcomment>
+        <translation>法语</translation>
+    </message>
+    <message>
+        <location filename="Qmli18n.qml" line="44"/>
+        <source>yuyan</source>
+        <translatorcomment>语言</translatorcomment>
+        <translation>语言</translation>
+    </message>
+</context>
+</TS>
